9666d3d22fda70166f0f63f59dab0b64577a144c
HWJ <hwj20010504@163.com>
2024-03-19 11:16:05 +0800

    添加DateTime扩展方法

---
+++ Utils/Hwj.Aow.Utils.CommonHelper/Extensions/Extensions.DateTime.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Hwj.Aow.Utils.CommonHelper.Extensions
+{
+    public static partial class Extensions
+    {
+        #region ToUnixTime
+
+        /// <summary>
+        /// dateTime转换为unixTime
+        /// </summary>
+        /// <param name="this"></param>
+        /// <returns></returns>
+        public static int ToUnixTime(this DateTime @this)
+        {
+            var startTime = TimeZoneInfo.ConvertTime(new DateTime(1970, 1, 1), TimeZoneInfo.Local);
+            return (int)(@this - startTime).TotalSeconds;
+        }
+
+        #endregion ToUnixTime
+    }
+}
